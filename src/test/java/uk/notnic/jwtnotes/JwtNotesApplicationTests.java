d8f6956b2e0fc704605292a5af9c8e520d6a1e20
notnic <nicovo989@gmail.com>
2023-09-27 12:56:25 +0100

    Initial Commit

---
+++ src/test/java/uk/notnic/jwtnotes/JwtNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.notnic.jwtnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
